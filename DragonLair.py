0caff2c7b536c3523cbad6fd208ab87e3e082ea9
Kelsey Hawley <k.karin.hawley@gmail.com>
2015-11-27 13:16:28 -0800

    adding in regex's, and basic logic in get_list & __parse_html

--- DragonLair.py
+++ DragonLair.py
@@ -11,6 +11,14 @@ class DragonLair:
     Class to list all dragons in lair, and associated familiars
     """
 
+    #regex globals
+    dragon_url_patt = re.compile(r'main\.php\?p=lair(?:&|&amp;)id=95470'
+                                 r'(?:&|&amp;)tab=dragon(?:&|&amp;)'
+                                 r'did=(?P<dragon_id>\d+)')
+    familiar_equipped_patt = re.comiple(r'/images/icons/famicon.png')
+
+    lair_max_page = 1
+
     def __init__(self, lair_id, fr_cookie, verbose=False):
         """
         :param string lair_id: Id of dragon lair
@@ -28,6 +36,10 @@ class DragonLair:
             'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
             self.fr_cookie,
         ]
+        self.lair_url = "http://flightrising.com/main.php?p=lair&id=" +\
+                        self.lair_id + "&page="
+        self.dragon_url = "http://flightrising.com/main.php?p=lair&id=" + \
+                          self.lair_id + "&tab=dragon&did="
 
     def echo(self, msg, newline=False):
         """If verbose, print the msg.
@@ -42,4 +54,42 @@ class DragonLair:
             sys.stdout.write(msg)
 
     def get_list(self):
+        i = 1
+        while i <= self.lair_max_page:
+            html = MyCurl.curl(self.lair_url, self.send_headers)
+            self.__parse_lair_page(html)
+            i += 1
         pass
+
+    def __parse_lair_page(self, html):
+
+        soup = BeautifulSoup(html, "html.parser").select("#super-container")[0]
+
+        if self.lair_max_page == 1:
+            # may not have been set, go find it in the page
+            self.lair_max_page = max([int(i) for i in
+                                      re.findall(r'(\d+)',
+                                                 soup.find_all("div")[4].text)])
+
+        dragon_cards = soup.select(".dragoncard")
+
+        if len(dragon_cards) != 15:
+            sys.stderr.write("Error: Something happened, did not find 15 dragons on page")
+
+        for dragon_card in dragon_cards:
+            a_tag = dragon_card.find("a")
+            match = re.search(self.dragon_url_patt, a_tag.attrs["href"])
+            if match:
+                dragon_id = match.group("dragon_id")
+
+            if dragon_card.selector(".loginbar")[0]\
+                .find(self.__locate_familiar_equipped_img()):
+                has_familiar = True
+
+
+
+        pass
+
+    def __locate_familiar_equipped_img(self, tag):
+        return tag.has_attr("src") and \
+               re.search(self.familiar_equipped_patt, tag.attrs["src"])
