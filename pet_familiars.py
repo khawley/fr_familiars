d8f56cee1109f2a12e88263e9e74bfa0bd64d7ca
Kelsey Hawley <k.karin.hawley@gmail.com>
2015-11-26 19:52:16 -0800

    removed the curl function & added in MyCurl.

--- pet_familiars.py
+++ pet_familiars.py
@@ -1,19 +1,9 @@
 #! /bin/python
 import re
 import sys
-import pycurl
 from bs4 import BeautifulSoup
-from bs4.element import NavigableString
-try:
-    from io import BytesIO
-except ImportError:
-    from StringIO import StringIO as BytesIO
-try:
-    # python 3
-    from urllib.parse import urlencode
-except ImportError:
-    # python 2
-    from urllib import urlencode
+
+from MyCurl import MyCurl
 
 fr_cookie = 'Cookie: PHPSESSID=askldfjlkanfln; userid=alsdflanf; user_key=1234567890; username=test;'
 dragon_id = '11902870'
@@ -88,11 +78,11 @@ def pet_beast(b_id):
         'X-Requested-With: XMLHttpRequest',
     ]
     # for id in ids:
-    result = parse_response(curl(url, send_headers, {"id": b_id}))
+    result = parse_response(MyCurl.curl(url, send_headers, {"id": b_id}))
 
     if result["msg"] == "not equipped":
         url = 'http://flightrising.com/includes/familiar_active.php?id=' + str(dragon_id) + '&itm=' + str(b_id)
-        result = parse_response(curl(url, send_headers))
+        result = parse_response(MyCurl.curl(url, send_headers))
         if result["msg"] == "not_equipped":
             sys.stderr.write("Error: Tried to equip familiar id " + str(b_id) +
                              " but still failed to 'pet'")
@@ -175,79 +165,6 @@ def parse_rewards(div):
     }
 
 
-def header_function(header_line):
-    # HTTP standard specifies that headers are encoded in iso-8859-1.
-    # On Python 2, decoding step can be skipped.
-    header_line = header_line.decode('utf-8')
-
-    # Header lines include the first status line (HTTP/1.x ...).
-    # We are going to ignore all lines that don't have a colon in them.
-    # This will botch headers that are split on multiple lines...
-    if ':' not in header_line:
-        return
-
-    # Break the header line into header name and value.
-    name, value = header_line.split(':', 1)
-
-    # Remove whitespace that may be present.
-    # Header lines include the trailing newline, and there may be whitespace
-    # around the colon.
-    name = name.strip()
-    value = value.strip()
-
-    # Header names are case insensitive.
-    # Lowercase name here.
-    name = name.lower()
-
-    # Now we can actually record the header name and value.
-    headers[name] = value
-
-
-def curl(url, send_headers=[], post_data={}):
-    global headers
-    headers = {}
-
-    buffer = BytesIO()
-
-    c = pycurl.Curl()
-    c.setopt(pycurl.URL, url)
-    if send_headers:
-        c.setopt(pycurl.HTTPHEADER, send_headers)
-    if post_data:
-        # post_data = {'field': 'value'}
-        # Form data must be provided already urlencoded.
-        postfields = urlencode(post_data)
-        # Sets request method to POST,
-        # Content-Type header to application/x-www-form-urlencoded
-        # and data to send in request body.
-        c.setopt(c.POSTFIELDS, postfields)
-    c.setopt(c.WRITEDATA, buffer)
-    c.setopt(c.HEADERFUNCTION, header_function)
-    # c.setopt(c.VERBOSE, True)
-    c.perform()
-    c.close()
-
-    # Figure out what encoding was sent with the response, if any.
-    # Check against lowercased header name.
-    encoding = None
-    if 'content-type' in headers:
-        content_type = headers['content-type'].lower()
-        match = re.search('charset=(\S+)', content_type)
-        if match:
-            encoding = match.group(1)
-            # print('Decoding using %s' % encoding)
-    if encoding is None:
-        # Default encoding for HTML is iso-8859-1.
-        # Other content types may have different default encoding,
-        # or in case of binary data, may have no encoding at all.
-        encoding = 'iso-8859-1'
-        # print('Assuming encoding is %s' % encoding)
-
-    body = buffer.getvalue()
-    # Decode using the encoding we figured out.
-    return body.decode(encoding)
-
-
-main()
-# pet_beast(413)
+# main()
+pet_beast(413)
 # print parse_response(response)
