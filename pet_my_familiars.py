ac28d3aba391550f5b3aef1e428766c1cb72774c
Kelsey Hawley <k.karin.hawley@gmail.com>
2015-11-27 00:05:25 -0800

    actual program script in a separate file, with the fr_cookie and dragon_id

---
+++ pet_my_familiars.py
@@ -0,0 +1,16 @@
+from PetFamiliars import PetFamiliars
+from Bestiary import Bestiary
+
+my_fr_cookie = 'Cookie: PHPSESSID=askldfjlkanfln; userid=alsdflanf; ' \
+               'user_key=1234567890; username=test;'
+dragon_id = '11902870'
+
+# B = Bestiary(fr_cookie=my_fr_cookie, verbose=True)
+# B.get_list()
+# B.print_beasts_breakdown()
+
+# pf = PetFamiliars(fr_cookie=my_fr_cookie,
+#                   equip_dragon=dragon_id, verbose=True)
+# pf.get_bestiary()
+# pf.pet_my_familiars()
+# pf.print_taming_breakdown()
