5ca33c2537ddabfff0d37b9b6fb87984200382ce
Kelsey Hawley <k.karin.hawley@gmail.com>
2015-11-27 11:49:21 -0800

    added a settings file & example to load in the Cookie and other settings

--- pet_my_familiars.py
+++ pet_my_familiars.py
@@ -1,16 +1,25 @@
+import sys
 from PetFamiliars import PetFamiliars
 from Bestiary import Bestiary
 
-my_fr_cookie = 'Cookie: PHPSESSID=askldfjlkanfln; userid=alsdflanf; ' \
-               'user_key=1234567890; username=test;'
-dragon_id = '11902870'
+try:
+    from settings import FR_COOKIE, DRAGON_ID, LAIR_ID
+except ImportError:
+    sys.stderr.write("*ERROR: settings.py has not been configured.\n"
+                     "        Please see settings_example.py for help.\n")
+    exit()
 
-# B = Bestiary(fr_cookie=my_fr_cookie, verbose=True)
+if not FR_COOKIE:
+    sys.stderr.write("*ERROR: Please add FR_COOKIE to inputs.py\n"
+                     "        See settings_example.py for help.\n")
+    exit()
+
+# B = Bestiary(fr_cookie=FR_COOKIE, verbose=True)
 # B.get_list()
 # B.print_beasts_breakdown()
 
-# pf = PetFamiliars(fr_cookie=my_fr_cookie,
-#                   equip_dragon=dragon_id, verbose=True)
+# pf = PetFamiliars(fr_cookie=FR_COOKIE,
+#                   equip_dragon=DRAGON_ID, verbose=True)
 # pf.get_bestiary()
 # pf.pet_my_familiars()
 # pf.print_taming_breakdown()
---
+++ settings_example.py
@@ -0,0 +1,31 @@
+"""
+ Rename this file to inputs.py, will not be saved in git
+ fill in your settings like the examples below.
+ the cookie can be copied from your web-browser,
+ and may be or contain more fields longer than below
+"""
+
+
+"""
+ Required: This is the login credentials for your account.
+    DO NOT USE YOUR USERNAME & PASSWORD.  Grab the Cookie Header.
+ Example:
+    'Cookie: PHPSESSID=askldfjlkanfln; userid=alsdflanf; ' \
+    'user_key=1234567890; username=test;'
+"""
+FR_COOKIE = ''
+
+
+"""
+  Optional: dragon to use to equip familiars that are in Hoard
+  Example: '0123456789'
+"""
+DRAGON_ID = ''
+
+
+"""
+  Optional: lair id will check when a familiar has gotten a gold,
+    and swap for an unequipped familiar, then send to vault
+  Example: '0123456789'
+"""
+LAIR_ID = ''
