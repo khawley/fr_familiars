46d779a33c3c898c16968bb78b2070a6624a6d58
Kelsey Hawley <k.karin.hawley@gmail.com>
2015-11-28 11:40:07 -0800

    added a basic program to pet_my_familiars.py.  I hope to tweak this and refine this, possibly into a command line program

--- pet_my_familiars.py
+++ pet_my_familiars.py
@@ -1,6 +1,7 @@
 import sys
 from PetFamiliars import PetFamiliars
 from Bestiary import Bestiary
+from DragonLair import DragonLair
 
 try:
     from settings import FR_COOKIE, DRAGON_ID, \
@@ -15,12 +16,18 @@ if not FR_COOKIE:
                      "        See settings_example.py for help.\n")
     exit()
 
-# B = Bestiary(fr_cookie=FR_COOKIE, verbose=True)
-# B.get_all()
-# B.print_beasts_breakdown()
+if not DRAGON_LIST:
+    D = DragonLair(LAIR_ID, FR_COOKIE, verbose=True)
+    DRAGON_LIST = D.get_list()
 
-# pf = PetFamiliars(fr_cookie=FR_COOKIE,
-#                   equip_dragon=DRAGON_ID, verbose=True)
-# pf.get_bestiary()
-# pf.pet_my_familiars()
-# pf.print_taming_breakdown()
+if not BESTIARY_BREAKDOWN:
+    B = Bestiary(fr_cookie=FR_COOKIE, verbose=True)
+    BESTIARY_BREAKDOWN = B.get_all()
+
+pf = PetFamiliars(fr_cookie=FR_COOKIE,
+                  equip_dragon=DRAGON_ID,
+                  bestiary_breakdown=BESTIARY_BREAKDOWN,
+                  dragon_list=DRAGON_LIST,
+                  verbose=True)
+pf.pet_my_familiars()
+pf.print_taming_breakdown()
