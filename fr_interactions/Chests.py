b3b2d96dff81daeb3d2f51de6a3bbace74cd5a8a
Kelsey Hawley <k.karin.hawley@gmail.com>
2015-12-06 19:36:48 -0800

    Bulk of the Chests Class being committed

--- fr_interactions/Chests.py
+++ fr_interactions/Chests.py
@@ -1,113 +1,207 @@
-# data includes id of chest, and ver=conn, the 'confirmed' click to open
-# page= not needed for it to work
+import re
+from bs4 import BeautifulSoup
+
+from fr_interactions.FrBase import FrBase
+from fr_interactions.fr_constants import GILDED_CHEST_ID, IRON_CHEST_ID,\
+    RUSTED_CHEST_ID
 
 
-# iron treasure chest - which id #575
-"""
-curl 'http://flightrising.com/includes/ol/openchest.php' -H 'Cookie: PHPSESSID=test' -H 'Origin: http://flightrising.com' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'Accept: */*' -H 'Referer: http://flightrising.com/main.php?p=hoard&tab=other&page=2' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --data 'id=575&ver=con&page=2' --compressed
-"""
+class Chests(FrBase):
+    all_chest_results = {}
+    raw_results = {"gilded": [], "iron": [], "rusted": []}
+    item_map = {}
+    item_totals = {}
 
+    # regex patterns
+    item_ajax_url_patt = re.compile(r"includes/itemajax\.php\?"
+                                    r"id=(?P<item_id>\d+)"
+                                    r"&tab=(?P<item_type>\w+)")
+    treasure_pile_img_patt = re.compile(r"treasure_pile")
+    gem_pile_img_patt = re.compile(r"gem_pile")
+    item_ajax_response_patt = re.compile(r'(?P<item_name>[\w\s]+)[\n\r\t]+'
+                                         r'(?P<item_type>[^\n]+)[\n\t\r]+'
+                                         r'Sell Value: (?P<sell_value>\d+)')
 
+    def __init__(self, fr_cookie, gilded_qty=0, iron_qty=0,
+                 rusted_qty=0, item_map=None, verbosity=0):
+        """
+        :param str fr_cookie: Cookie that has login information
+        :param int gilded_qty: number of gilded chests to open
+        :param int iron_qty: number of iron chests to open
+        :param int rusted_qty: number of rusted chests to open
+        :param dict item_map: map from previous run, dict of item_ids as keys,
+            and descriptive dicts as values
+        :param int verbosity: level of error messages to print
+        :return:
+        """
+        try:
+            self.gilded_qty = int(gilded_qty) \
+                if type(gilded_qty) is str else gilded_qty
+            self.iron_qty = int(iron_qty) \
+                if type(iron_qty) is str else iron_qty
+            self.rusted_qty = int(rusted_qty)\
+                if type(rusted_qty) is str else rusted_qty
+        except ValueError:
+            self.error("Error: parameters not of type int", True)
+            exit()
+        self.item_map = item_map or self.item_map
+        FrBase.__init__(self, fr_cookie, verbosity)
 
-"""
-        <div style="text-align:left; margin-bottom:15px; font-size:11px;">You found the following items:</div>
-		<div style="width:475px; margin-left:auto; margin-right:auto; text-align:center;">
+    def open_all_chests(self):
+        self.open_gilded_chests(self.gilded_qty)
+        self.open_iron_chests(self.iron_qty)
+        self.open_rusted_chests(self.rusted_qty)
 
-					<span style="width:85px; position:relative; display:inline-block;">
-				<a rel="includes/itemajax.php?id=208&tab=trinket" class="clue">
-					<img src="/images/cms/trinket/208.png" />
-					<div style="font-size:12px; color:#731d08; font-weight:bold;">
-					2					</div>
-				</a>
-			</span>
-						<span style="width:85px; position:relative; display:inline-block;">
-				<a rel="includes/itemajax.php?id=6349&tab=trinket" class="clue">
-					<img src="/images/cms/trinket/6349.png" />
-					<div style="font-size:12px; color:#731d08; font-weight:bold;">
-					4					</div>
-				</a>
-			</span>
-						<span style="width:85px; position:relative; display:inline-block;">
-				<a rel="includes/itemajax.php?id=283&tab=equipment" class="clue">
-					<img src="/images/cms/equipment/283.png" />
-					<div style="font-size:12px; color:#731d08; font-weight:bold;">
-					1					</div>
-				</a>
-			</span>
-						<span style="width:85px; position:relative; display:inline-block;">
-				<img src="/images/layout/treasure_pile.png" />
-				<div style="font-size:12px; color:#731d08; font-weight:bold;">
-				4110				</div>
-			</span>
-						<span style="width:85px; position:relative; display:inline-block;">
-				<img src="/images/layout/gem_pile.png" width="85" height="85" />
-				<div style="font-size:12px; color:#731d08; font-weight:bold;">
-				5				</div>
-			</span>
-					</div>
-		<div style="margin-top:15px; text-align:center; margin-bottom:10px;">
-			<button class="beigebutton thingbutton" id="no">Collect</button>
-		</div>
-        	<script type="text/javascript">
-	$(function(){
-		$('#no').click(function(e){
-			window.location ='main.php?p=hoard&tab=other&page=2';
-			$("#specialty").html('<img src="/images/layout/loading.gif"> loading...');
-		});
-	});
-	$('a.clue').cluetip({
-		height:"auto"
-	});
-	</script>
-"""
+    def open_gilded_chests(self, quantity=0):
+        """
 
+        :param int quantity:
+        :return:
+        """
+        # if not quantity, go until there is no more
+        for i in xrange(0, quantity):
+            result = self.open_one_chest(GILDED_CHEST_ID)
+            self.raw_results["gilded"].append(tuple(result))
 
-# rusted treasure chest #574
-"""curl 'http://flightrising.com/includes/ol/openchest.php' -H 'Cookie: PHPSESSID=test' -H 'Origin: http://flightrising.com' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'Accept: */*' -H 'Referer: http://flightrising.com/main.php?p=hoard&tab=other&page=2' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --data 'id=574&ver=con&page=2' --compressed"""
+    def open_iron_chests(self, quantity=0):
+        """
 
-"""
-        <div style="text-align:left; margin-bottom:15px; font-size:11px;">You found the following items:</div>
-		<div style="width:475px; margin-left:auto; margin-right:auto; text-align:center;">
+        :param int quantity:
+        :return:
+        """
+        # if not quantity, go until there is no more
+        for i in xrange(0, quantity):
+            result = self.open_one_chest(IRON_CHEST_ID)
+            self.raw_results["iron"].append(tuple(result))
 
-					<span style="width:85px; position:relative; display:inline-block;">
-				<a rel="includes/itemajax.php?id=1161&tab=trinket" class="clue">
-					<img src="/images/cms/trinket/1161.png" />
-					<div style="font-size:12px; color:#731d08; font-weight:bold;">
-					2					</div>
-				</a>
-			</span>
-						<span style="width:85px; position:relative; display:inline-block;">
-				<a rel="includes/itemajax.php?id=3538&tab=equipment" class="clue">
-					<img src="/images/cms/equipment/3538.png" />
-					<div style="font-size:12px; color:#731d08; font-weight:bold;">
-					1					</div>
-				</a>
-			</span>
-						<span style="width:85px; position:relative; display:inline-block;">
-				<a rel="includes/itemajax.php?id=7889&tab=trinket" class="clue">
-					<img src="/images/cms/trinket/7889.png" />
-					<div style="font-size:12px; color:#731d08; font-weight:bold;">
-					1					</div>
-				</a>
-			</span>
-						<span style="width:85px; position:relative; display:inline-block;">
-				<img src="/images/layout/treasure_pile.png" />
-				<div style="font-size:12px; color:#731d08; font-weight:bold;">
-				1775				</div>
-			</span>
-					</div>
-		<div style="margin-top:15px; text-align:center; margin-bottom:10px;">
-			<button class="beigebutton thingbutton" id="no">Collect</button>
-		</div>
-        	<script type="text/javascript">
-	$(function(){
-		$('#no').click(function(e){
-			window.location ='main.php?p=hoard&tab=other&page=2';
-			$("#specialty").html('<img src="/images/layout/loading.gif"> loading...');
-		});
-	});
-	$('a.clue').cluetip({
-		height:"auto"
-	});
-	</script>
-"""
+    def open_rusted_chests(self, quantity=0):
+        """
+
+        :param int quantity:
+        :return:
+        """
+        # if not quantity, go until there is no more
+        for i in xrange(0, quantity):
+            result = self.open_one_chest(RUSTED_CHEST_ID)
+            self.raw_results["rusted"].append(tuple(result))
+
+    def open_one_chest(self, chest_id):
+        # list of results from ONE chest
+        results = self.__parse_open_chest_response(
+            self.__curl_open_chest(chest_id))
+
+        # add these results to the main dict for tallying
+        for r in results:
+            self.item_totals[r["id"]] = self.item_totals.get(r["id"], 0) + \
+                                        int(r["quantity"])
+        return results
+
+    def __curl_open_chest(self, chest_id):
+        """
+
+        :param chest_id:
+        :return: html from curl
+        :rtype: str
+        """
+        url = "http://flightrising.com/includes/ol/openchest.php"
+        post_data = {
+            "id": chest_id,
+            "ver": "con"  # this is 'confirming' version
+        }
+        return self.curl(url, post_data=post_data)
+
+    def __parse_open_chest_response(self, html):
+        """
+
+        :param str html:
+        :return: list of result dicts
+            ex: [{"id":...,"type":...,"qty":...,"name":...},...]
+        :rtype: list
+        """
+        this_rewards = []
+        soup = BeautifulSoup(html, "html.parser")
+        for span in soup.find_all("span"):
+            # get qty in the <div>
+            quantity = span.find("div").text.strip()
+            # get a href for id + tab (type), also can use for more info
+
+            a_tag = span.find("a")
+            if a_tag:
+                match = re.search(self.item_ajax_url_patt,
+                              a_tag.attrs.get("rel", ""))
+                if match:
+                    result = self.__get_item_name(match.group("item_id"),match.group("item_type"))
+                    result["quantity"] = quantity
+
+                    this_rewards.append(result)
+
+            # not an item, likely treasure or gems
+            else:
+                img_src = span.find("img").attrs.get("src", "")
+                found = ""
+                # is treasure
+                if re.search(self.treasure_pile_img_patt, img_src):
+                    found = "treasure"
+                elif re.search(self.gem_pile_img_patt, img_src):
+                    found = "gem"
+
+                if found:
+                    this_rewards.append({
+                        "id": found,
+                        "type": found,
+                        "name": found,
+                        "quantity": quantity
+                    })
+
+        return this_rewards
+
+    def __get_item_name(self, item_id, item_type):
+        """
+
+        :param str item_id: id of the item
+        :param str item_type: type as specified in the ajax url
+            (may not match actual type, ex: equipment & apparel)
+        :return: dict of ajaxed results - id, name, type, ajax_url or {}
+        :rtype: dict
+        """
+        # look up in internal map first.  If there, simply return result
+        if self.item_map.get(item_id):
+            return self.item_map[item_id]
+
+        # query the itemajax for the name + description
+        url = "https://flightrising.com/includes/itemajax.php?id=" + \
+              item_id + "&tab=" + item_type
+
+        soup = BeautifulSoup(self.curl(url), "html.parser")
+        m = re.search(self.item_ajax_response_patt, soup.text)
+        if m:
+            result = {
+                "id": item_id,
+                "name": m.group("item_name"),
+                "type": m.group("item_type"),
+                "ajax_url": url
+
+            }
+            # add to internal dict for quicker querying next time
+            self.item_map[item_id] = result
+        return {}
+
+    def print_chest_results(self):
+        # make item_map only from item_totals, sort by type
+        sorted_item_map = sorted([self.item_map[item_id]
+                                  for item_id in self.item_totals.keys()
+                                  if item_id in self.item_map],
+                                 key=lambda x: x["type"])
+        print "Chest Results"
+        # print treasure + gems separately, since they aren't in the item_map
+        print "- Treasure:", self.item_totals["treasure"]
+        print "- Gems:", self.item_totals.get("gem", "0")
+        print
+
+        this_type = None
+        for item_dict in sorted_item_map:
+            if item_dict["type"] != this_type:
+                this_type = item_dict["type"]
+                print "===", item_dict["type"], "==="
+            print "-", item_dict["name"], self.item_totals[item_dict["id"]],
+            print "(id:" + item_dict["id"] + ")"
